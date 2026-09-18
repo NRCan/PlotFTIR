086e0fab86ee481e4a50083324cb6f2682a6f55f
Philip Bulsink <bulsinkp@gmail.com>
2026-09-18 10:49:20 -0400

    Remove in-code cli call

--- R/plot_ftir.R
+++ R/plot_ftir.R
@@ -75,10 +75,19 @@ plot_ftir_core <- function(
   }
 
   if (!requireNamespace("scales", quietly = TRUE)) {
-    cli::cli_abort(c(
-      "{.pkg PlotFTIR} requires {.pkg scales} package installation.",
-      i = "Install {.pkg scales} with {.run install.packages('scales')}"
-    ))
+    .pkg_abort(
+      list(
+        en = c(
+          "{.pkg PlotFTIR} requires {.pkg scales} package installation.",
+          i = "Install {.pkg scales} with {.run install.packages('scales')}"
+        ),
+        fr = c(
+          "{.pkg PlotFTIR} n\u00e9cessite l'installation du paquet {.pkg scales}.",
+          i = "Installez le paquet {.pkg scales} avec la commande {.run install.packages('scales')}"
+        )
+      ),
+      call = rlang::caller_env()
+    )
   }
 
   ftir <- check_ftir_data(ftir)
